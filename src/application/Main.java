ba74fcea2379ca05a4eb1fc745972fd7b5012ca0
Pedro Santos <pedrohssantos1509@gmail.com>
2025-07-17 18:53:58 -0300

    Initial commit: Começando projeto

---
+++ src/application/Main.java
@@ -0,0 +1,12 @@
+package application;
+
+import application.menu.Menu;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        String caminho = "formulario.txt";
+        Menu.menuInicial();
+    }
+}
---
+++ src/application/menu/Menu.java
@@ -0,0 +1,58 @@
+package application.menu;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Menu {
+
+    public static void menuInicial(){
+        int escolha = 0;
+        Scanner sc = new Scanner(System.in);
+        while (escolha < 1 || escolha > 6){
+            try {
+
+                System.out.println("1. Cadastrar um novo pet");
+                System.out.println("2. Alterar os dados do pet cadastrado");
+                System.out.println("3. Deletar um pet cadastrado");
+                System.out.println("4. Listar todos os pets cadastrados");
+                System.out.println("5. Listar pets por algum critério (idade, nome, raça)");
+                System.out.println("6. Sair");
+                System.out.print("Qual opção você deseja? ");
+
+                if (sc.hasNextInt()){
+                    escolha = sc.nextInt();
+
+                    if (escolha < 1 || escolha > 6){
+                        throw new InputMismatchException("\nEntrada inválida! Por favor digite um número válido pelo sistema!\n");
+                    }
+
+                } else {
+                    throw new InputMismatchException("\nEntrada inválida! Por favor digite um número válido pelo sistema!\n");
+                }
+
+
+            } catch (InputMismatchException e){
+                System.out.println("\nEntrada inválida! Por favor digite um número válido pelo sistema!\n");
+            }
+        }
+        sc.close();
+    }
+
+    public static void leituraFormulario(String caminho){
+        try (BufferedReader br = new BufferedReader(new FileReader(caminho))) {
+            String line = br.readLine();
+
+            while (line != null){
+                System.out.println(line);
+                line = br.readLine();
+            }
+        } catch (IOException e){
+            System.out.println("Erro: " + e.getMessage());
+        }
+    }
+
+
+}
